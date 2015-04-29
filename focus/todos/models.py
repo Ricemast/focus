6562e2d17dbd2d469daab399ad472fb223ac2525
Alex Price <alex@rocketpod.co.uk>
2015-04-29 08:23:05 +0100

    Made todos repriorities when new one is made with same priority

--- focus/todos/models.py
+++ focus/todos/models.py
@@ -3,15 +3,26 @@ from django.db import models
 
 class Todo(models.Model):
     text = models.CharField(max_length=200)
-    priority = models.IntegerField(unique=True)
+    priority = models.IntegerField()
     complete = models.BooleanField(default=False)
 
     @property
     def next(self):
-        """Return the next highest priority todo"""
+        """Return the next highest priority todo."""
         return Todo.objects.order_by('priority').filter(
             priority__gt=self.priority
         ).first()
 
     def __str__(self):
         return self.text
+
+    def save(self, *args, **kwargs):
+        """
+        Override the default save to push objects with the same priority
+        down.
+        """
+        todo = Todo.objects.filter(priority=self.priority).first()
+        if todo:
+            todo.priority += 1
+            todo.save()
+        super(Todo, self).save(*args, **kwargs)

b37d521c79938e891727a14d5fe1ebb483bdf211
Alex Price <alex@rocketpod.co.uk>
2015-05-23 12:53:12 +0100

    added up next (fixed #26)

--- src/pages/focus.js
+++ src/pages/focus.js
@@ -5,14 +5,26 @@ import {vars} from '../utils/variables';
 export class Focus {
     constructor() {
         this.todo = {};
+        this.nexttodo = {};
     }
 
     // Fetches and parses the todo JSON from the API
     fetchTodo(id) {
         return http.get(vars.todo_url(id)).then(response => {
             this.todo = JSON.parse(response.response);
+            this.fetchNext(this.todo.next);
         }, () => {
             alert('There was an error fetching the todo.');
+        });
+    }
+
+    // Fetches and parses the next todo
+    // TODO: DRY
+    fetchNext(id) {
+        return http.get(vars.todo_url(id)).then(response => {
+            this.nexttodo = JSON.parse(response.response);
+        }, () => {
+            alert('There was an error fetching the next todo.');
         });
     }
 
